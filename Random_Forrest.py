1751aa0969b67ac5e3d46378089f902ee334f23f
KyleAnthonyHay <haykyle917@gmail.com>
2023-11-25 11:23:55 -0500

    Random Forest p1

--- Random_Forrest.py
+++ Random_Forrest.py
@@ -4,18 +4,19 @@ from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.feature import StringIndexer, VectorAssembler
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
+print("Starting Spark Session")
+
 # Initialize a Spark session
 spark = SparkSession.builder.appName(
     "CancerDiagnosisRandomForest").getOrCreate()
 
 # Loading Dataset
-# Replace 'path_to_dataset' with the actual path to your CSV file
 data = spark.read.csv('./project3_data.csv', header=True, inferSchema=True)
 
-# Preprocessing
+# -------------- Preprocessing --------------
 # Convert categorical labels to numbers
 labelIndexer = StringIndexer(
-    inputCol="diagnosis", outputCol="indexedLabel").fit(data)
+    inputCol="diagnosis", outputCol="indexeddiagnosis").fit(data)
 
 # Assemble feature vectors (assuming all other columns are features)
 featureCols = data.columns
@@ -25,9 +26,10 @@ assembler = VectorAssembler(inputCols=featureCols, outputCol="features")
 # Split the data into training and test sets
 (trainData, testData) = data.randomSplit([0.7, 0.3])
 
-# Training the Random Forest Model
+# -------------- Training the Random Forest Model --------------
 # Define the Random Forest model
-rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="features")
+rf = RandomForestClassifier(
+    labelCol="indexeddiagnosis", featuresCol="features")
 
 # Chain indexers and forest in a Pipeline
 pipeline = Pipeline(stages=[labelIndexer, assembler, rf])
@@ -35,13 +37,14 @@ pipeline = Pipeline(stages=[labelIndexer, assembler, rf])
 # Train model
 model = pipeline.fit(trainData)
 
-# Evaluating the Model
-# Make predictions
+# -------------- Make predictions --------------
 predictions = model.transform(testData)
 
 # Select prediction, true label, and compute test error
 evaluator = MulticlassClassificationEvaluator(
-    labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    labelCol="indexeddiagnosis", predictionCol="prediction", metricName="accuracy")
 accuracy = evaluator.evaluate(predictions)
 
+
+# -------------- Print Results --------------
 print("Test Accuracy = %g" % (accuracy))
